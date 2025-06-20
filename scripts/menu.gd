8fee92d3b704b39c709c6c66b88684324fd30164
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-20 20:20:57 +0800

    main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
