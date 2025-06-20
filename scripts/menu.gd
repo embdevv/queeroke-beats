95be7b15a911f1f4cd5bc388a62e5a219e70b185
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-20 20:46:58 +0800

    added options

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,18 @@
 extends Control
+@onready var options: Panel = $Options
 
+func _ready() -> void:
+	options.hide()
+	print("hello")
+	
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/songSelect.tscn")
 
 func _on_options_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options.tscn")
+	options.show()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+	
+func _on_back_button_pressed() -> void:
+	options.hide()
