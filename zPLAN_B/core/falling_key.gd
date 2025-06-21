3715507756a7511bb17321e1197a3f0c64e40df6
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-22 03:41:03 +0800

    Adjust falling key parameters and update level layout
    Reduced falling key speed and adjusted pass threshold in falling_key.gd. Enabled edit mode and updated level info and music in level_editor.gd. Modified level_one.tscn to reposition UI elements, adjust camera zoom, and update panel and key positions for improved alignment.

--- zPLAN_B/core/falling_key.gd
+++ zPLAN_B/core/falling_key.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@export var fall_speed: float = 400
+@export var fall_speed: float = 300
 var frame: int = 0
 
 var init_y_pos: float = -360
 
 var has_passed: bool = false
-var pass_threshold = 300.0
+var pass_threshold = 340.0
 
 func _init():
 	set_process(false)
@@ -15,7 +15,7 @@ func _process(delta):
 	global_position += Vector2(0, fall_speed * delta)
 	
 	# 1.368
-	if global_position.y > 280.0 and not $Timer.is_stopped():
+	if global_position.y > 340.0 and not $Timer.is_stopped():
 		#print($Timer.wait_time - $Timer.time_left)
 		$Timer.stop()
 		has_passed = true
