322dc862bc6428e918696e35cb40f9591243e091
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-22 01:08:01 +0800

    Add combo system and update UI for score feedback
    Introduced IncrementCombo and ResetCombo signals to support a combo system. Updated game_ui.gd and game_ui.tscn to display combo count, and adjusted score label positioning and formatting. Modified key_listener.gd to emit combo signals on successful and missed key presses, and improved score feedback display. Cleaned up score_press_text.gd by removing unused code and adjusted related scene files for better visual alignment.

--- zPLAN_B/core/key_listener.gd
+++ zPLAN_B/core/key_listener.gd
@@ -26,6 +26,12 @@ func _process(delta):
 		if falling_key_queue.front().has_passed:
 			falling_key_queue.pop_front()
 			
+			var st_inst = score_text.instantiate()
+			get_tree().get_root().call_deferred("add_child", st_inst)
+			st_inst.setTextInfo("MISS")
+			st_inst.global_position = global_position + Vector2(0, -30)
+			Signals.ResetCombo.emit()
+			
 		if Input.is_action_just_pressed(key_name):
 			var key_to_pop = falling_key_queue.pop_front()
 			
@@ -35,24 +41,26 @@ func _process(delta):
 			if distance_from_pass < perfect_press_threshold:
 				Signals.IncrementScore.emit(perfect_press_score)
 				press_score_text = "PERFECT"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < great_press_threshold:
 				Signals.IncrementScore.emit(great_press_score)
 				press_score_text = "GREAT"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < good_press_threshold:
 				Signals.IncrementScore.emit(good_press_score)
 				press_score_text = "GOOD"
+				Signals.IncrementCombo.emit()
 			elif distance_from_pass < ok_press_threshold:
 				Signals.IncrementScore.emit(ok_press_score)
 				press_score_text = "OKAY"
+				Signals.IncrementCombo.emit()
 			else:
 				press_score_text = "MISS"
+				Signals.ResetCombo.emit()
 			
 			key_to_pop.queue_free()
 			
-			var st_inst = score_text.instantiate()
-			get_tree().get_root().call_deferred("add_child", st_inst)
-			st_inst.setTextInfo(press_score_text)
-			st_inst.global_position = global_position + Vector2(0, -20)
+
 	
 func createFallingKey():
 	var fk_inst = falling_key.instantiate()
