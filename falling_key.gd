9325d8db1fe2a649c1bfbdebe9795839ed6fa0cd
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-21 19:29:23 +0800

    Add falling key instantiation on beat input
    Updated beat_listener.gd to instantiate and position a falling key when the 'don_hit' action is pressed. Refactored falling_key.gd to support setup and process control, and adjusted scene files for improved positioning and scaling.

--- falling_key.gd
+++ falling_key.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
 @export var speed: float = 500.0
+var init_x_pos: float = 545.0
+
+func _init():
+	set_process(false)
 
 func _process(delta):
-	position += Vector2(-speed * delta, 0)
+	global_position += Vector2(-speed * delta, 0)
 	
-	if position.x > 80.0 and not $Timer.is_stopped():
+	if global_position.x > 80.0 and not $Timer.is_stopped():
 		print($Timer.wait_time - $Timer.time_left)
 		$Timer.stop()
+
+func Setup(target_y: float):
+	position = Vector2(init_x_pos, target_y)
+	set_process(true)
+	
--- scenes/ui/beat_listener.gd
+++ scenes/ui/beat_listener.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 @export var falling_key = preload("res://scenes/ui/falling_key.tscn")
-@export var key_name: String = ""
 
 func _process(delta):
-	if Input.is_action_just_pressed(key_name):
-		print(key_name)
+	if Input.is_action_just_pressed("don_hit"):
+		createFallingKey()
+
+func createFallingKey():
+	var beat_inst = falling_key.instantiate()
+	get_tree().get_root().call_deferred("add_child", beat_inst)
+	beat_inst.Setup(position.y)
