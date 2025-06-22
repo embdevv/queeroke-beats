18dc9e80203c2de817d668d65539be2791c56337
Padre <madison.suero@gmail.com>
2025-06-22 17:44:12 +0800

    Update menu.gd

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,7 +6,6 @@ extends Control
 func _ready() -> void:
 	options.hide()
 	calibration_opts.hide()
-	print("hello, world!")
 	
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://zPLAN_B/level/song_select.tscn")
