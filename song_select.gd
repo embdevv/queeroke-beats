be592b5d44649fca4a90d7840ff7121b67f5b34a
embdevv <111500829+embdevv@users.noreply.github.com>
2025-06-22 18:01:33 +0800

    Add animated end screen and song select descriptions
    Implemented a rolling score animation and result reveal on the end screen, added background music to the end screen, and updated the back button to return to the main menu. Enhanced the song select screen with hover descriptions for each level and added necessary UI elements. Also included the new victoryEnd.mp3 audio asset.

--- song_select.gd
+++ song_select.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var description_label: RichTextLabel = $Panel/Panel/descriptionLabel
 
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://zPLAN_B/level/level_one.tscn")
@@ -7,3 +8,27 @@ func _on_level_1_pressed() -> void:
 
 func _on_level_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://zPLAN_B/level/level_two.tscn")
+
+func _ready():
+	description_label.visible = false
+
+func _on_level_1_mouse_entered() -> void:
+	description_label.text = "NEW CLOTHESSSSS"
+	description_label.visible = true
+
+func _on_level_1_mouse_exited() -> void:
+	description_label.visible = false
+
+func _on_level_2_mouse_entered() -> void:
+	description_label.text = "WLW: No assets yet :3 but you can play it"
+	description_label.visible = true
+
+func _on_level_2_mouse_exited() -> void:
+	description_label.visible = false
+
+func _on_level_3_mouse_entered() -> void:
+	description_label.text = "Coming soon :3"
+	description_label.visible = true
+
+func _on_level_3_mouse_exited() -> void:
+	description_label.visible = false
--- zPLAN_B/level/end_screen.gd
+++ zPLAN_B/level/end_screen.gd
@@ -4,16 +4,45 @@ extends Control
 @onready var combo_label: RichTextLabel = $ComboLabel
 @onready var result_label: RichTextLabel = $ResultLabel
 
+var rolling_duration := 5
+var rolling_timer := 0.0
+var showing_actual := false
+
 func _ready():
-	score_label.text = "%d" % GameStats.final_score
-	combo_label.text = "Max Combo  : %d" % GameStats.final_combo
+	
+	rolling_timer = 0.0
+	showing_actual = false
+	score_label.text = "[center]---[/center]"
+	result_label.text = "[center]Calculating results...[/center]"
+	
+	#score_label.text = "[center] %d [/center]" % GameStats.final_score
+	#combo_label.text = "Max Combo  : %d" % GameStats.final_combo
+
+func _process(delta):
+	
+	if not showing_actual:
+		rolling_timer += delta
+		if rolling_timer >= rolling_duration:
+			showing_actual = true
+			show_actual_results()
+		else:
+			show_random_results()
+
+func show_random_results():
+	var random_score = randi() % 99999
+	
+	score_label.text = "[center] %d [/center]" % random_score
+	result_label.text = "[center][color = yellow]Calculating...[/color] [/center]"
+
+func show_actual_results():
+	score_label.text = "[center] %d [/center]" % GameStats.final_score
 	
 	if GameStats.final_score >= 20000:
-		result_label.text = "[center] [color=gold] Amazing! You are truly a star [/center][/color]"
+		result_label.text = "[center] [color=gold] Amazing! You are truly a star [/center][/color][/center]"
 	elif GameStats.final_score < 20000:
 		result_label.text = "[center] [color=silver] Good job! Let's aim for a higher one next time [/color] [/center]"
 	else:
 		result_label.text = "[center][color=bronze]Keep practicing![/color][/center]"
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://zPLAN_B/level/song_select.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
